28c65d41dbba28c5d2912074044cc221b0380926
red-ninja-warrior <sharminjose@gmail.com>
2020-02-06 01:18:34 -0600

    After the Documentation is setup

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -298,6 +298,7 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        'ps-react': path.resolve(__dirname, '../src/components'),
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
@@ -314,7 +315,7 @@ module.exports = function(webpackEnv) {
         // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
         // please link the files into your node_modules/ and let module-resolution kick in.
         // Make sure your source files are compiled, as they will not be processed in any way.
-        new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+        // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
       ],
     },
     resolveLoader: {
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Custom message */
+const ExampleHelloWorld = () => {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
+
+export default ExampleHelloWorld;
